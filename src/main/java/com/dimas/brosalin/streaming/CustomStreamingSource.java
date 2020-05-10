50f215285b88a2607e57f068c404af52c25a6060
DimasBro0110 <dimasgitacc@gmail.com>
2020-05-10 15:34:58 +0300

    + StreamSourceProvider trait

--- src/main/java/com/dimas/brosalin/streaming/CustomStreamingSource.java
+++ src/main/java/com/dimas/brosalin/streaming/CustomStreamingSource.java
@@ -1,11 +1,11 @@
 package com.dimas.brosalin.streaming;
 
 import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.sources.BaseRelation;
-import org.apache.spark.sql.sources.DataSourceRegister;
-import org.apache.spark.sql.sources.RelationProvider;
-import org.apache.spark.sql.sources.SchemaRelationProvider;
+import org.apache.spark.sql.execution.streaming.Source;
+import org.apache.spark.sql.sources.*;
 import org.apache.spark.sql.types.StructType;
+import scala.Option;
+import scala.Tuple2;
 import scala.collection.immutable.Map;
 
 import java.util.Optional;
@@ -13,7 +13,7 @@ import java.util.Optional;
 /**
  * Created by DmitriyBrosalin on 10/05/2020.
  */
-public class CustomStreamingSource implements RelationProvider, SchemaRelationProvider, DataSourceRegister {
+public class CustomStreamingSource implements RelationProvider, SchemaRelationProvider, StreamSourceProvider, DataSourceRegister {
 
     @Override
     public BaseRelation createRelation(SQLContext sqlContext, Map<String, String> parameters) {
@@ -32,6 +32,16 @@ public class CustomStreamingSource implements RelationProvider, SchemaRelationPr
 
     @Override
     public BaseRelation createRelation(SQLContext sqlContext, Map<String, String> parameters, StructType schema) {
+        return null;
+    }
+
+    @Override
+    public Tuple2<String, StructType> sourceSchema(SQLContext sqlContext, Option<StructType> schema, String providerName, Map<String, String> parameters) {
+        return null;
+    }
+
+    @Override
+    public Source createSource(SQLContext sqlContext, String metadataPath, Option<StructType> schema, String providerName, Map<String, String> parameters) {
         return null;
     }
 }
